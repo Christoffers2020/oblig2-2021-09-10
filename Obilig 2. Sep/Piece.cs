dea9eae15820cec79412bb72a9f085fbd231e0a2
Get Academy <76997222+Christoffers2020@users.noreply.github.com>
2021-09-10 12:40:11 +0200

    Add project files.

---
+++ Obilig 2. Sep/Piece.cs
@@ -0,0 +1,14 @@
+﻿namespace Obilig_2._Sep
+{
+    abstract class Piece
+    {
+        public string Symbol { get; }
+
+        protected Piece(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public abstract bool Move(string fromPosition, string toPosition);
+    }
+}
